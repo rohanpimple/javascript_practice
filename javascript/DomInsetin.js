fcba2f8436691a354ea4a930c220638c9b4f8980
Rohanpimple <pimplerohan55@gmail.com>
2025-01-13 12:40:04 +0530

    add change

---
+++ javascript/DomInsetin.js
@@ -0,0 +1,47 @@
+//INSERTION ELEMENTS
+
+//let el = document.createElement("div")
+
+// let newel = document.createElement("button");
+// newel.innerText = 'click me';
+// console.log(newel);
+
+// adds at the end of node (inside)
+
+// let div = document.querySelector("div");
+// div.append(newel);
+
+// add at the start of node(inside)
+
+// let div = document.querySelector("div");
+// div.prepend(newel);
+
+// add before the node 
+
+// let div = document.querySelector("div");
+// div.before(newel);
+
+// add after the node
+
+// let div = document.querySelector("div");
+// div.after(newel);
+
+// DELETION OF NODE 
+
+// let par = document.querySelector("p");
+// par.remove();
+
+// heading.remove
+
+// ******************** PRACTICE *********************** //
+
+// let newbtn = document.createElement("button");
+// newbtn.innerText = "Enter me";
+// newbtn.style.color = "white";
+// newbtn.style.backgroundColor = "red";
+// document.querySelector("body").prepend(newbtn);
+
+// ******************** PRACTICE *********************** //
+
+let par = document.querySelector("p");
+par.classList.add(para2);
